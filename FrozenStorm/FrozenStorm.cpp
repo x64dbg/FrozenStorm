57427af2a017daec63fb206d9bd67150c5cfe860
mrexodia <mr.exodia.tpodt@gmail.com>
2016-08-28 15:18:23 +0200

    initial few hash functions

---
+++ FrozenStorm/FrozenStorm.cpp
@@ -0,0 +1,68 @@
+#include "FrozenStorm.h"
+#include <openssl/mdc2.h>
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+
+#define HASH_ALLOC(length) *hash = (unsigned char*)frozen_alloc(*hashsize = length)
+#define HDATA (const unsigned char*)data
+#define HSIZE datasize
+#define HHASH *hash
+
+void* frozen_alloc(size_t size)
+{
+    return GlobalAlloc(GMEM_FIXED, size);
+}
+
+void frozen_free(void* ptr)
+{
+    GlobalFree(ptr);
+}
+
+void hash_mdc2(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(MDC2_DIGEST_LENGTH);
+    MDC2(HDATA, HSIZE, HHASH);
+}
+
+void hash_md4(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(MD4_DIGEST_LENGTH);
+    MD4(HDATA, HSIZE, HHASH);
+}
+
+void hash_md5(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(MD5_DIGEST_LENGTH);
+    MD5(HDATA, HSIZE, HHASH);
+}
+
+void hash_sha1(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(SHA_DIGEST_LENGTH);
+    SHA1(HDATA, HSIZE, HHASH);
+}
+
+void hash_sha224(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(SHA224_DIGEST_LENGTH);
+    SHA224(HDATA, HSIZE, HHASH);
+}
+
+void hash_sha256(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(SHA256_DIGEST_LENGTH);
+    SHA256(HDATA, HSIZE, HHASH);
+}
+
+void hash_sha384(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(SHA384_DIGEST_LENGTH);
+    SHA384(HDATA, HSIZE, HHASH);
+}
+
+void hash_sha512(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize)
+{
+    HASH_ALLOC(SHA512_DIGEST_LENGTH);
+    SHA512(HDATA, HSIZE, HHASH);
+}
--- FrozenStorm/FrozenStorm_hash.cpp
+++
@@ -1,6 +0,0 @@
-#include "FrozenStorm.h"
-
-FROZEN_API void* frozen_alloc(size_t size)
-{
-    return nullptr;
-}
