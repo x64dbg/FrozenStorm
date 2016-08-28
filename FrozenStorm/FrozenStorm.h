9f8f5b5e904887d4fb8c18f6ccb39bb8e1373295
mrexodia <mr.exodia.tpodt@gmail.com>
2016-08-28 14:50:38 +0200

    interface

--- FrozenStorm/FrozenStorm.h
+++ FrozenStorm/FrozenStorm.h
@@ -1,4 +1,16 @@
 #ifndef FROZENSTORM_H
 #define FROZENSTORM_H
 
+#ifdef BUILD_FROZENSTORM
+#define FROZEN_API extern "C" __declspec(dllexport)
+#else
+#define FROZEN_API extern "C" __declspec(dllimport)
+#endif //BUILD_FROZENSTORM
+
+FROZEN_API void* frozen_alloc(size_t size);
+FROZEN_API void frozen_free(void* ptr);
+
+FROZEN_API void hash_md5(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize);
+FROZEN_API void hash_sha1(const void* data, size_t datasize, unsigned char** hash, size_t* hashsize);
+
 #endif //FROZENSTORM_H
---
+++ FrozenStorm/FrozenStorm_hash.cpp
@@ -0,0 +1,6 @@
+#include "FrozenStorm.h"
+
+FROZEN_API void* frozen_alloc(size_t size)
+{
+    return nullptr;
+}
